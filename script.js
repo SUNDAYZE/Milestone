ee07356fe937d57e2bcac8979dbcd58685f129d7
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2022-12-05 20:10:08 -0800

    Added a greeting, score count, and animation for both characters

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //Add a jumping function for kirby in order to avoid the bad guys
 var kirby =document.getElementById('kirby');
-var enemy = document.getElementById("enemy");
+var enemy = document.getElementById('enemy');
 var counter=0;
 
 function jump(){
-    if(kirby.classList == "animate"){return}
-    kirby.classList.add("animate");
+    if(kirby.classList == 'animate'){return}
+    kirby.classList.add('animate');
     setTimeout(function(){
-        kirby.classList.remove("animate");
+        kirby.classList.remove('animate');
     },300);
 }
+
+var checkDead = setInterval(function() {
+    let kirbyTop = parseInt(window.getComputedStyle(kirby).getPropertyValue("top"));
+    let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
+    if(enemyLeft<20 && enemyLeft>-20 && kirbyTop>=130){
+        enemy.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        enemy.style.animation = "enemy 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
+
