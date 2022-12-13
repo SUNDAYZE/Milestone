894964e91e9f2c6ba2a809daa409ced94f632862
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2022-12-12 18:22:04 -0800

    Last changes

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 //Add a jumping function for kirby in order to avoid the bad guys
-var kirby =document.getElementById('kirby');
+var kirby = document.getElementById('kirby');
 var enemy = document.getElementById('enemy');
-var counter=0;
+var counter = 0;
 
-function jump(){
-    if(kirby.classList == 'animate'){return}
+function jump() {
+    if (kirby.classList == 'animate') { return }
     kirby.classList.add('animate');
-    setTimeout(function(){
+    setTimeout(function () {
         kirby.classList.remove('animate');
-    },300);
+    }, 300);
 }
 
-var checkDead = setInterval(function() {
+//Keep score count and gameover popup
+var checkDead = setInterval(function () {
     let kirbyTop = parseInt(window.getComputedStyle(kirby).getPropertyValue("top"));
     let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
-    if(enemyLeft<20 && enemyLeft>-20 && kirbyTop>=130){
+    if (enemyLeft < 20 && enemyLeft > -20 && kirbyTop >= 130) {
         enemy.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
         enemy.style.animation = "enemy 1s infinite linear";
-    }else{
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
 
